363f17b85562e0f4054574db2f25f94da3470b5c
Mahfuj Ruzel <mahfujruzel@gmail.com>
2025-04-24 02:58:46 -0400

    new file

---
+++ Python Exercises/BasicPython/Lab1.py
@@ -0,0 +1,45 @@
+print("name: Mahfuj Ruzel")
+print("age: 38")
+print("hobby: Reading, watching")
+
+name = input("Mahfuj Ruzel: ")
+print("Hello, " + name + "!")
+
+# This program prints the first five multiples of 3
+for i in range(1, 6):
+    print(i * 3)  # Multiply the current number by 3
+
+    # Addition
+print("3 + 5 =", 3 + 5)
+
+# Subtraction
+print("10 - 2 =", 10 - 2)
+
+# Multiplication
+print("4 * 7 =", 4 * 7)
+
+# Division
+print("15 / 3 =", 15 / 3)
+
+# Exponentiation
+print("2 ** 3 =", 2 ** 3)
+
+age = 39
+current_year = 2025
+birth_year = current_year - age
+print("Your birth year is:", birth_year)
+
+length = float(input("5: "))
+width = float(input("6: "))
+area = length * width
+print("The area of the rectangle is:", area)
+
+x = 5        # Integer
+y = 3.14     # Float
+name = "John" # String
+is_active = True  # Boolean
+
+print("x is of type:", type(x))
+print("y is of type:", type(y))
+print("name is of type:", type(name))
+print("is_active is of type:", type(is_active))
